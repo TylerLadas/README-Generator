9edfe22b5a2cbd502812b17cacfb4736d63a01f6
TylerLadas <tylerladas@hotmail.com>
2021-07-15 13:45:56 -0400

    add license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 const renderLicenseBadge = license => {
   if (license === "none") {
-    return ""
+    return "";
   } else {
     return `![License](https://img.shields.io/badge/License-${license}-<blue>)`;
   }
@@ -11,34 +11,74 @@ const renderLicenseBadge = license => {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 const renderLicenseLink = license => {
-
+  if (license === "none") {
+    return "";
+  } else {
+    return `\n* [License](#license)\n`;
+  }
 };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 const renderLicenseSection = license => {
-
+  if (license === "none") {
+    return "";
+  } else {
+    return `## License
+    
+    Licensed for use with the ${data.license} license.`;
+  }
 };
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = data => {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
 ## Description
+
+${data.description}
 
 ## Table of Contents
 
+* [Installation](#installation)
+
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
 ## Installation
+
+To install program please run:
+${data.installation}
 
 ## Usage
 
-## License
+${data.usage}
+
+${renderLicenseSection(data.license)}
 
 ## Contributing
 
+${data.contribution}
+
 ## Tests
 
+To test, please run:
+${data.test}
+
 ## Questions
+
+To view my other work, go to: 
+[${data.github}](https://github.com/${data.github})
+
+For questions and inquiries contact me at:
+${data.email}
 
 `;
 }
